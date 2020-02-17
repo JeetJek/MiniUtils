f40f881780f15487a51a7b08fda5082825f0c725
Andrew <kaibazakov@yandex.ru>
2020-02-17 22:40:34 +0700

    Добавьте файлы проекта.

---
+++ WebApplication2/Controllers/BaseController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class BaseController : Controller
+    {
+        [Route("/")]
+        public IActionResult Index()
+        {
+            return View("Views/Index.cshtml");
+        }
+        [Route("Dupes")]
+        public IActionResult DupeFinder()
+        {
+            return View("views/DupeFinder.cshtml");
+        }
+
+        [Route("/Dupes/find")]
+        public IActionResult Dupes(string STR1,string STR2)
+        {
+            return Ok();
+        }
+    }
+}
